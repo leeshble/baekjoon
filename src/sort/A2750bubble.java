119f461fc473827e859c6da6da23776bad764dd2
LeeSangHyun <leeshble@gmail.com>
2022-01-15 20:47:33 +0900

    버블 정렬

---
+++ src/sort/A2750bubble.java
@@ -0,0 +1,48 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class A2750bubble {
+
+	static int input;
+	static int[] array;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		input = Integer.parseInt(br.readLine());
+		array = new int[input];
+		for (int i = 0; i < input; i++) {
+			array[i] = Integer.parseInt(br.readLine());
+		}
+		br.close();
+		bubbleSort();
+		for (int val : array) {
+			sb.append(val).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	
+	static void bubbleSort() {
+		int temp;
+		for(int i = input - 1; i > 0; i--){	//뒤에서부터 정렬되므로 반대로 반복
+			for(int j = 0; j < i; j++){
+				// 버블 형식을 따라 정렬
+				if(array[j] > array[j+1]){
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
+	}
+}
